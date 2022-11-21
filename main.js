aa4ac65ce90ba9da9ebddacd721d0baee35c3ba3
altezzaxl <aleksandr.ilin186@gmail.com>
2022-11-21 18:33:52 +0300

    2

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+price_2 = 0
 document.querySelector("#stuff_1").onclick = function(){
-    alert("Вы нажали на кнопку");
-    
+    price = document.getElementById("order_price");
+    price_2 = price_2 + 100;
+    price.innerHTML = price_2 + "Р";
+
+
   }
+
